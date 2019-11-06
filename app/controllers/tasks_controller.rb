39a5034f1b693b4f3ade942c68f423d36f28ce64
LouiswuYJ <louiswu0327@gmail.com>
2019-11-06 15:49:39 +0800

    Basic CRUD done/Add Bootstrap 4

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path, notice: '編輯任務成功！'
+    else
+      render :edit  
+    end
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path, notice: '新增任務成功！'
+    else 
+      render :new
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path, notice: '刪除任務成功！'
+  end
+
+  private 
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+
+  def find_params
+    @task = Task.find(params[:id])
+  end
+end
