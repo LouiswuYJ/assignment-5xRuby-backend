51a02a1906fe767b60396ca2de78826c73fdf20b
LouiswuYJ <louiswu0327@gmail.com>
2019-11-08 16:23:16 +0800

    Add tags at index and _form / Tags getter, setter method in Task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,7 +39,7 @@ class TasksController < ApplicationController
 
   private 
   def task_params
-    params.require(:task).permit(:title, :description, :priority, :start_time, :end_time)
+    params.require(:task).permit(:title, :description, :priority, :start_time, :end_time, :tag_list)
   end
 
   def find_params
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,18 @@ class Task < ApplicationRecord
 
   enum priority: { low: 0, middle: 1, high: 2 }
   enum status: { pending: 0, processing: 1, done: 2 }
+
+  # tag_list 的 getter
+  # .map(&:name) 等於 .map{|x| x.name } 
+  def tag_list
+    tags.map(&:name).join(', ')
+  end
+
+  # tag_list 的 setter 
+  # 找到該Task下所有的tags後用split分開成陣列，再針對每個element去做first_or_create (Find the First Instance in the Table. If None Exists, Create One.)
+  def tag_list=(names)
+    self.tags = names.split(',').map do |item|
+      Tag.where(name: item.strip).first_or_create!
+    end
+  end
 end
