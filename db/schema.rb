084668d4e5e395ab660f6e6bbd8388827b04fcbb
LouiswuYJ <louiswu0327@gmail.com>
2019-11-08 16:27:31 +0800

    Remove column tag from Task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_070551) do
+ActiveRecord::Schema.define(version: 2019_11_08_082545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2019_11_08_070551) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.integer "priority", default: 0
-    t.integer "tag"
     t.integer "status", default: 0
   end
 
