35ba006f2223f4d12d5a5415c80e3e974464e5b0
LouiswuYJ <louiswu0327@gmail.com>
2019-11-13 10:06:13 +0800

    Fix task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  #寫法一 subject(:task)
   describe '#title' do
     #要測的主體(subject)為task，用FactoryBot產出假資料
     subject(:task) { create(:task) }  
@@ -14,24 +13,6 @@ RSpec.describe Task, type: :model do
       #覆寫原來的task
       subject(:task) { Task.new }
       it { is_expected.to_not be_valid }
-    end
-  end
-
-  #寫法二 subject{ task.title }
-  describe '#title' do
-    let(:task) { create(:task) }
-    subject { task.title }
-    it { is_expected.to eq 'sport' }
-
-    it 'is valid' do
-      expect(task).to be_valid
-    end
-
-    context "when title is nil" do
-      let(:task) { Task.new }
-      it "is invalid" do
-        expect(task).to_not be_valid
-      end
     end
   end
 end
