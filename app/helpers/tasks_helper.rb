14d5c4ed4c4a0a9654c6c1db95d12bbbd37929b6
LouiswuYJ <louiswu0327@gmail.com>
2019-11-14 19:44:12 +0800

    Fix cannot create task bug

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -7,30 +7,16 @@ module TasksHelper
     end
   end
 
-  def task_enum
-    Task.priorities.keys
+  def priority_selection
+    [["低", 'low'], ["中", "middle"], ["高", "high"]]
   end
 
   def task_status(status)
-    case status
-      when "pending"
-        t('views.status.pending')
-      when "processing"
-        t('views.status.processing')
-      when "done"
-        t('views.status.done')
-    end
+    t("views.status.#{status}")
   end
 
   def task_priority(priority)
-    case priority
-      when "low"
-        t('views.priority.low')
-      when "middle"
-        t('views.priority.middle')
-      when "high"
-        t('views.priority.high')
-    end
+    t("views.priority.#{priority}")
   end
 end
 
